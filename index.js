fbeee2e1b0f22bea9d8497dd8c0906995e62dbcf
DanielSiilva <daniellsousa0614@gmail.com>
2024-03-28 17:20:50 -0300

    feat: adcionando agregate:

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const functions = require("@google-cloud/functions-framework");
 
-const uri = process.env.MONGOBD_URL;
+const uri = process.env.MONGODB_UR;
 const client = new MongoClient(uri);
 
 functions.http("helloHttp", async (req, res) => {
   const authHeader = req.headers.authorization;
   const collectionName = req.body.collection;
-  const query = req.body.query;
+  const aggregationPipeline = req.body.aggregation; // Usar um pipeline de agregação
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Not authorized!" });
@@ -20,22 +20,24 @@ functions.http("helloHttp", async (req, res) => {
     return res.status(401).json({ message: "Not authorized" });
   }
 
-  if (!collectionName || !query) {
+  if (!collectionName || !aggregationPipeline) {
+    // Certifique-se de que o pipeline de agregação foi passado
     return res.status(400).json({
       message:
-        "It is necessary to pass collection and query the body the request",
+        "It is necessary to pass collection and aggregation pipeline in the body of the request",
     });
   }
 
   try {
     await client.connect();
     const collection = client.db("test").collection(collectionName);
-    const data = await collection.find(query).toArray();
+    const data = await collection.aggregate(aggregationPipeline).toArray(); // Use o método aggregate()
 
     res.json({
       data,
     });
   } catch (error) {
+    console.error("Error executing aggregation query:", error);
     res
       .status(500)
       .json({ message: "Internal server error", error: error.message });
